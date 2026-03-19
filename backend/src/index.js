bfdc51b2fb7271847f6b2c17dee2f1007e8b907e
harshitmaster8851 <harshitpersonal8851@gmail.com>
2026-03-19 22:53:03 +0530

    added backend phase 1 code

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// health route
+app.get("/health", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
